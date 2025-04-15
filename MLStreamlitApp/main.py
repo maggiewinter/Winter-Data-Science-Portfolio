2ae80d264bfa8956e709c2fb262214e9f39298db
Maggie Winter <mwinter@nd.edu>
2025-04-14 20:36:20 -0400

    Remove unnecessary libraries

--- MLStreamlitApp/main.py
+++ MLStreamlitApp/main.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
